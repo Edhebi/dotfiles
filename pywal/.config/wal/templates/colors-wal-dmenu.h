ad2fbe7ecd486a9a0b0ad020377c2d2e8b3cf51b
Edhebi <aurore53000@gmail.com>
2019-02-11 10:39:26 +0100

    added dwm

--- pywal/.config/wal/templates/colors-wal-dmenu.h
+++
@@ -1,5 +0,0 @@
-static const char *colors[SchemeLast][2] = {{
-	[SchemeNorm] = {{ "{color15}", "{color0}" }},
-	[SchemeSel]  = {{ "{color15}", "{color1}" }},
-	[SchemeOut]  = {{ "{color15}", "{color14}" }},
-}};
