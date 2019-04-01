274b5ed09e2a8fee99558f485a176d5303e8159e
Edhebi <aurore53000@gmail.com>
2019-04-02 00:28:00 +0200

    increased font size, added padding

--- st/repo/st/config.h
+++ st/repo/st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Hack Nerd Font Mono:size=13:antialias=true:autohint=true";
-static int borderpx = 0;
+static char *font = "Hack Nerd Font Mono:size=18:antialias=true:autohint=true";
+static int borderpx = 5;
 
 /*
  * What program is execed by st depends of these precedence rules:
